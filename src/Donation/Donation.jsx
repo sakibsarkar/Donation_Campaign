9bbe356cbfb088b83dba2d0aaea9d359f2f0a785
sakibsarkar <sakibsarkar707@gmail.com>
2023-09-25 14:44:00 +0600

    see all button added with functionality

--- src/Donation/Donation.jsx
+++ src/Donation/Donation.jsx
@@ -6,11 +6,37 @@ import { ImSad } from "react-icons/im";
 const Donation = () => {
 
     const [myDonations, SetMyDonations] = useState([])
+    const [isSeeAll, setIsSeeAll] = useState(false);
+    const [isHidden, setIsHidden] = useState(true);
+
+    const showAll = () => {
+        setIsSeeAll(!isSeeAll)
+        setIsHidden(!isHidden)
+
+    }
     useEffect(() => {
         const donations = JSON.parse(localStorage.getItem("donation"))
-        SetMyDonations(donations)
+        const extractedDonations = [...donations]
 
-    }, [])
+        if (isSeeAll) {
+            SetMyDonations(donations)
+            return
+        }
+
+        if (donations.length > 4) {
+            SetMyDonations(extractedDonations.splice(0, 4))
+            setIsHidden(false)
+
+        }
+        else {
+            SetMyDonations(donations)
+
+        }
+
+
+    }, [isSeeAll])
+
+
     return (
         <div className="wrappper">
             {
@@ -22,12 +48,25 @@ const Donation = () => {
                     </div>
 
                     :
+                    <div>
 
-                    <div className="myDonations">
-                        {
-                            myDonations.map((donation, id) => <MyDonations key={id} donation={donation}></MyDonations>)
-                        }
+                        <div className="myDonations">
+                            {
+                                myDonations.map((donation, id) => <MyDonations key={id} donation={donation}></MyDonations>)
+                            }
+
+                        </div>
+
+                        <div className="seeAll">
+                            <button style={{ display: `${isHidden ? "none" : "block"}` }} onClick={showAll}>See all..</button>
+
+                        </div>
+
+
+
+
                     </div>
+
             }
         </div >
     );
